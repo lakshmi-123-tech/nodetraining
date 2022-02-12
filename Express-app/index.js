63ec844d64ccdbbf21fa2717590e61a9cf439f67
lakshmiprasanna <prasanna.kasireddy18@gmail.com>
2022-02-12 14:14:54 +0530

    second push

--- Express-app/index.js
+++ Express-app/index.js
@@ -1,8 +1,14 @@
 const express=require("express");
-const server=express();
+const server = express();
+const userauthroutes = require("./user/routes/user-auth-route");
+
 server.listen(3200,()=>{
-    console.log("server is listening")
-})
+    console.log("server is listening");
+});
+
+server.use("/api/user/auth", userauthroutes);
+
 server.get("/",(req,res)=>{
-    res.end("hello from server, you are connected")
+    res.end("hello from server, you are connected.");
 })
+
